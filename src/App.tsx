f0b428e9e2d061d2a8bb811f8a34f7a1dd4187b2
elpwc/wniko <elpwc@hotmail.com>
2021-11-25 14:51:43 +0800

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { Layout, Menu } from 'antd';
+import { Header } from 'antd/lib/layout/layout';
 import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './pages/Blogs';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+        <Layout >
+          <Header className="header" >
+            <div className="logo" />
+            <Menu theme="light" mode="horizontal" defaultSelectedKeys={['home']}>
+              <Menu.Item key="home"><Link to='/'>首页</Link></Menu.Item>
+              <Menu.Item key="projects"><Link to='/projects'>项目</Link></Menu.Item>
+              <Menu.Item key="blogs"><Link to='/blog'>博客</Link></Menu.Item>
+            </Menu>
+          </Header>
+        </Layout>
+
+        <Routes>
+          <Route index element={<Home />} ></Route>
+          <Route path='projects' element={<Projects />}></Route>
+          <Route path='blog' element={<Blogs />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Project.tsx
@@ -0,0 +1,13 @@
+interface P{
+    name: string;
+    url: string;
+    githubUrl: string;
+}
+
+export default function Project(props: P){
+    return(<>
+        <p>{props.name}</p>
+        <p>{props.url}</p>
+        <p>{props.githubUrl}</p>
+    </>);
+}
