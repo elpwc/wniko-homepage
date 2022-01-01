1e4d8e3fb0db25c1faeb0295a259a777328cce45
elpwc/wniko <elpwc@hotmail.com>
2022-01-02 03:27:39 +0800

    F: BlogEdit 1

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 export default {
   url: 'http://localhost:3001/api/v1',
   project: '/projects',
+  blog: '/blogs',
 };
--- src/pages/BlogEdit.tsx
+++ src/pages/BlogEdit.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Space } from 'antd';
+import { Button, Divider, Space, Input, Select } from 'antd';
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Navigate, useLocation, useParams } from 'react-router';
@@ -9,6 +9,12 @@ import Page404 from './404';
 import { LeftOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import './blogView.css';
+import RightContent from '../components/RightContent';
+import { PlusOutlined } from '@ant-design/icons';
+import LangUtils from '../lang/langUtils';
+
+const { TextArea } = Input;
+const { Option } = Select;
 
 interface P {
   update: boolean;
@@ -18,6 +24,9 @@ interface P {
 export default function BlogEdit(props: P) {
   const params = useParams();
   const mylocation = useLocation();
+
+  const [markdownCode, setMarkdownCode]: [string, any] = useState('');
+  const [newSubjectName, setNewSubjectName]: [string, any] = useState('');
 
   let currentBlogid: number = 0;
   let currentBlog: Blog = BlogUtils.create('', '');
@@ -29,6 +38,14 @@ export default function BlogEdit(props: P) {
 
   currentBlogid = Number(params.blogid);
 
+  const onDraftClick = () => {};
+
+  const onSaveClick = () => {};
+
+  const onNewSubjectClick = () => {};
+
+  const onHeadImageClick = () => {};
+
   return (
     <>
       <div style={{ position: 'absolute', top: '15px', left: '-50px' }}>
@@ -39,10 +56,74 @@ export default function BlogEdit(props: P) {
         </Link>
       </div>
 
+      <RightContent update={props.update} setUpdate={props.setUpdate}>
+        <div style={{ backgroundColor: 'white', borderRadius: '5px', marginTop: '5px' }}>
+          <Space direction='vertical'>
+          <Select
+            style={{ width: '100%' }}
+            placeholder='选择主题'
+            dropdownRender={(menu) => (
+              <div>
+                <Divider style={{ margin: '4px 0' }} />
+                <div style={{ display: 'flex', flexWrap: 'nowrap', padding: 8 }}>
+                  <Input
+                    style={{ flex: 'auto' }}
+                    value={newSubjectName}
+                    onChange={(e) => {
+                      setNewSubjectName(e.target.value);
+                    }}
+                  />
+                  <a style={{ flex: 'none', padding: '8px', display: 'block', cursor: 'pointer' }} onClick={onNewSubjectClick}>
+                    <PlusOutlined /> 添加主题
+                  </a>
+                </div>
+              </div>
+            )}
+          >
+            {/*items.map((item) => (
+              <Option key={item}>{item}</Option>
+            ))*/}
+          </Select>
+
+          <Select style={{ width: '100%' }} placeholder='选择语言'>
+            {LangUtils.getEnumStrings().map((lang: string) => {
+              return (
+                <Option key={lang} value={lang}>
+                  {LangUtils.enumStrToLangName(lang)}
+                </Option>
+              );
+            })}
+          </Select>
+
+          <Input placeholder='地点'></Input>
+
+          <Select style={{ width: '100%' }} placeholder='选择可访问性'>
+            {['private', 'urasekai', 'public'].map((value: string) => {
+              return (
+                <Option key={value} value={value}>
+                  {value}
+                </Option>
+              );
+            })}
+          </Select>
+          </Space>
+
+        </div>
+      </RightContent>
+
       <div style={{ backgroundColor: 'white', borderRadius: '5px', padding: '20px 50px', marginTop: '8px' }}>
-        <h1>{currentBlog.title}</h1>
         <Space>
-          <p className='bloginfo'>{currentBlog.author}</p>
+          <Button onClick={onHeadImageClick}>选择头图</Button>
+          <Button onClick={onDraftClick}>保存草稿</Button>
+          <Button onClick={onSaveClick}>发布</Button>
+        </Space>
+        <h1>
+          <Input placeholder='标题' bordered={false} style={{ fontWeight: 'bold', fontSize: '20px', borderBottom: 'solid 1px lightgray' }} />
+        </h1>
+        <Space>
+          <p className='bloginfo'>
+            <Input placeholder='作者' bordered={false} style={{ borderBottom: 'solid 1px lightgray' }} />
+          </p>
           <p className='bloginfo'>
             创建：
             {
@@ -65,7 +146,25 @@ export default function BlogEdit(props: P) {
 
         <Divider style={{ marginTop: '10px', marginBottom: '20px' }} />
 
-        <ReactMarkdown children={currentBlog.content} />
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <TextArea
+            rows={20}
+            style={{ width: '50%' }}
+            onChange={(e) => {
+              setMarkdownCode(e.target.value);
+            }}
+          />
+          <div
+            style={{
+              border: 'solid 1px gray',
+              marginLeft: '5px',
+              padding: '5px',
+              width: '50%',
+            }}
+          >
+            <ReactMarkdown children={markdownCode} />
+          </div>
+        </div>
       </div>
     </>
   );
