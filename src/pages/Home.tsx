7d0e6ceb2c7997ee9096833214b3833878890dde
elpwc/wniko <elpwc@hotmail.com>
2022-02-09 18:15:01 +0800

    F: TimeLine base

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import { Timeline } from 'antd';
 import { useEffect } from 'react';
+import TimeLine from '../components/TimeLine';
 import { CurrentPageStorage } from '../dataStorage/storage';
 
 interface P {
@@ -11,5 +13,10 @@ export default function Home(props: P) {
     CurrentPageStorage.set('home');
     props.setUpdate();
   }, []);
-  return <>home</>;
+
+  return (
+    <>
+      <TimeLine update={props.update} setUpdate={props.setUpdate} />
+    </>
+  );
 }
