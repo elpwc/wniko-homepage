0a52f36428f8f5293da8d43c5254bc613d06e6c2
elpwc/wniko <elpwc@hotmail.com>
2024-10-04 16:04:07 +0900

    feat: home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Timeline } from 'antd';
 import { useEffect } from 'react';
 import TimeLine from '../components/TimeLine';
 import { CurrentPageStorage } from '../dataStorage/storage';
+import './home.css';
 
 interface P {
   update: boolean;
@@ -15,8 +16,23 @@ export default function Home(props: P) {
   }, []);
 
   return (
-    <>
-      <TimeLine update={props.update} setUpdate={props.setUpdate} />
-    </>
+    <div>
+      <p className="headerText1">Wniko or うに</p>
+      <div id="welcomeContainer">
+        <div className="welcomeItem" id="welcomeContent1">
+          <p>大学院在读</p>
+          <p>地理，地図，ｱﾌﾟﾘ開発，言語，漢字，旅行，アニメ，ヲタ芸，音ゲ</p>
+          <p></p>
+        </div>
+        <div className="welcomeItem">
+          <p className="headerText2">Contact</p>
+          <p>📫elpwc@hotmail.com</p>
+          <p>🐱elpwc</p>
+        </div>
+      </div>
+      <a className="twitter-timeline" href="https://twitter.com/elpwc?ref_src=twsrc%5Etfw">
+        Tweets by elpwc
+      </a>
+    </div>
   );
 }
