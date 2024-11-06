d87c7931dc704f4dc2b7dd9baa7c4e666b3749dc
elpwc/wniko <elpwc@hotmail.com>
2024-11-06 11:09:53 +0900

    fix: server url

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -5,7 +5,7 @@ export const IS_DEBUG = false;
 export default {
   usingLanguages: [Lang.en, Lang.ja, Lang.zh_cn, Lang.zh_tw],
   api: {
-    url: IS_DEBUG ? 'http://localhost:8001/api/v1' : 'http://localhost:8001/api/v1',
+    url: IS_DEBUG ? 'http://localhost:8002/api/v1' : 'https://www.elpwc.com/server/homepage/api/v1',
     project: '/projects',
     blog: '/blogs',
     blogsubject: '/blog-subjects',
